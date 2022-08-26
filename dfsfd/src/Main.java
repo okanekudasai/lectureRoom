a805e68ff8253bc3bc5f5d56ce76485996d9bf65
abcd <ss@ss.ss>
2022-08-26 10:04:19 +0900

    최단경로

--- dfsfd/src/Main.java
+++ dfsfd/src/Main.java
@@ -1,11 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {	
+class Node {
+	int next;
+	int weight;
+	Node(int next, int weight) {
+		this.next = next;
+		this.weight = weight;
+	}
+}
+
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -13,42 +22,30 @@ public class Main {
 		int v = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(br.readLine());
-		ArrayList<int []> [] graph = new ArrayList [v+1];
-		int [] answer = new int [v+1];
-		Arrays.fill(answer, 2000000);
-		for (int i = 0; i < e; i++) {
+		PriorityQueue<Node> [] pq = new PriorityQueue [v+1];
+		for(int i = 0; i < e; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			if (graph[a] == null) graph[a] = new ArrayList<int []>();
-			int [] temp = {b, c}; graph[a].add(temp);
+			if (pq[a] == null) pq[a] = new PriorityQueue<Node>((o1, o2) -> {return o1.weight - o2.weight;});
+			pq[a].offer(new Node(b, c));
 		}
-		boolean [] visit = new boolean [v+1];
+		int [] answer = new int [v+1];
+		Arrays.fill(answer, -1);
 		answer[k] = 0;
-		int min_point = 0;
-		int min_value;
-		int cnt = 0;
-		while (min_point != -1 && graph[k] != null) {
-			cnt++;
-			visit[k] = true;
-			min_point = -1;
-			min_value = Integer.MAX_VALUE;
-			for (int [] i : graph[k]) {
-				answer[i[0]] = Math.min(answer[i[0]], answer[k] + i[1]);
-				if (visit[i[0]] == false && min_value > answer[i[0]]) {
-					min_value = answer[i[0]];
-					min_point = i[0];
-				}
+		while (pq[k].size() > 0) {
+			Node temp = pq[k].poll();
+			if (answer[temp.next] != -1) continue;
+			answer[temp.next] = temp.weight;
+			if (pq[temp.next] == null) continue;
+			for (Node node : pq[temp.next]) {
+				pq[k].offer(new Node(node.next, temp.weight + node.weight));
 			}
-			System.out.println(Arrays.toString(answer));
-			k = min_point;
-			System.out.println(k);
-			if (cnt >= 4) break;
 		}
-//		for (int i = 1; i <= v; i++) {
-//			if (answer[i] >= 2000000) System.out.println("INF");
-//			else System.out.println(answer[i]);
-//		}
+		for (int i = 1; i <= v; i++) {
+			if (answer[i] == -1) System.out.println("INF");
+			else System.out.println(answer[i]);
+		}
 	}
 }
